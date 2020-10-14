c6da0b92cac060250e1de80850dacb42713267a1
amalthea-code <amalthea22@yandex.ru>
2020-10-14 20:35:34 +0400

    d2

---
+++ site3/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        speed: 0,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
