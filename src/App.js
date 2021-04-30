bdd5a2456846bde616e41435aff60011dc80eed3
Amalthea <amalthea22@yandex.ru>
2021-04-30 16:04:35 +0400

    add Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Header from './components/Header/Header';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
+    <div>
+      <Home />
     </div>
+      
+    
   );
 }
 
---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import Header from './../../components/Header/Header';
+import Container from '@material-ui/core/Container';
+
+const Home = () => {
+    return (
+        <Container maxWidth="sm">
+            <Header />
+        </Container>
+    )
+
+}
+
+export default Home;
